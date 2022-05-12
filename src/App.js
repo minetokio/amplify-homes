ff0fbe9fcbaba8b993c59bb03d221ba8b95d2908
toki-mine <toki-mine@foresight.co.jp>
2022-05-12 22:52:10 +0900

    spike.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-
+import { ActionCardCollection, NavBar, MarketingFooter } from './ui-components'
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+return (
+
+
+<div className="App">
+<NavBar />
+<ActionCardCollection  />
+<MarketingFooter />
+</div>
+
+
+);
 }
 
 export default App;
